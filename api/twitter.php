1c2a908d9c315aeb0f3dc01ba240f608e99289c5
İzni Burak Demirtaş <izniburak@outlook.com>
2015-07-02 05:47:52 +0300

    Update twitter.php

--- api/twitter.php
+++ api/twitter.php
@@ -1,19 +1,19 @@
-<?php 
-/* ******************************************************************************
-	Developer: Ýzni Burak Demirtaþ (@izniburak)
-	Web: http://burakdemirtas.org
-	Project: jQuery jTwit
-	Version: 1.0
-	Project Page: http://burakdemirtas.org/projects/jquery.jtwit/
-	Older Version: http://coda.co.za/content/projects/jquery.twitter/
-	License: GNU General Public License (http://www.gnu.org/copyleft/gpl.html)
-******************************************************************************* */
+<?php
+/*
+* Developer: izni burak demirtaÅŸ (@izniburak)
+* Web: http://burakdemirtas.org
+* Project: jQuery jTwit
+* Version: 1.0
+* Project Page: http://burakdemirtas.org/projects/jquery.jtwit/
+* Older Version: http://coda.co.za/content/projects/jquery.twitter/
+* License: GNU General Public License (http://www.gnu.org/copyleft/gpl.html) 
+*/
 
 ###  SETTINGS  ###
 /* 
-	How to create an application?
-	Visit http://dev.twitter.com
-	Help? -> http://burakdemirtas.org/projects/jquery.jtwit/twitter-api/
+*	How to create an application?
+*	Visit http://dev.twitter.com
+*	Help? -> http://burakdemirtas.org/projects/jquery.jtwit/twitter-api/
 */
 $settings = array(
 	'oauth_access_token' => "YOUR oAuth ACCESS TOKEN KEY",
@@ -25,8 +25,9 @@ $settings = array(
 ###  DO NOT EDIT THIS SECTION  ###
 if (empty($_SERVER['HTTP_REFERER']) && 'twitter.php' == basename($_SERVER['SCRIPT_FILENAME'])) exit();
 require_once('TwitterAPIExchange.php'); extract($_GET);
-$url			= 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-$getfield		= '?screen_name='.$screen_name.'&count='.$count.'&include_rts='.$include_rts.'&excludeReplies='.$excludeReplies;
+$url		= 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+$getfield	= '?screen_name='.$screen_name.'&count='.$count.'&include_rts='.$include_rts.'&excludeReplies='.$excludeReplies;
 $requestMethod	= 'GET';
-$twitter		= new TwitterAPIExchange($settings);
-$data			= $twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest(); echo $data;
+$twitter	= new TwitterAPIExchange($settings);
+$data		= $twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest();
+echo $data;
